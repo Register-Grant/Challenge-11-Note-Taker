7c34c849535cb9a3126d773e017519aae6862f03
Grant Register <register.grant@gmail.com>
2022-07-29 17:39:31 -0600

    cleaned it up a little bit - still need to generate a readme and post to heroku

--- server.js
+++ server.js
@@ -11,18 +11,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
+//all my get routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
-});
-
 app.get('/api/notes', (req, res) => {
-    res.json(db.slice(1))
+  res.json(db.slice(1))
 });
 
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+
+//as I create new notes, they get pushed to the end of a previously empty array
 const createNote = (body, notesArray) => {
     const newNote = body;
     if(!Array.isArray(notesArray)) {
@@ -46,25 +48,6 @@ app.post('/api/notes', (req, res) => {
     const newNote = createNote(req.body, db)
     res.json(newNote)
 });
-
-const deleteNote = (id, notesArray) => {
-    for(var i = 0; i < notesArray.length; i++) {
-      let note = notesArray[i];
-      if(note.id == id) {
-        notesArray.splice(i, 1);
-        fs.writeFileSync(
-          path.join(__dirname, './db/db.json'),
-          JSON.stringify(notesArray, null, 2)
-        );
-        break;
-      }
-    }
-  };
-
-  app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, db);
-    res.json(true);
-  });
 
   app.listen(PORT, () => {
     console.log(`I CAN HEAR YOU on port ${PORT}...`)
